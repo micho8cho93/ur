043ec316522cddaee92f79916524f08391547f1a
micho8cho93 <182514846+micho8cho93@users.noreply.github.com>
2026-03-13 07:02:15 +0100

    Fix Tailwind at rule warning

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts = Array.from(new Set([...config.resolver.assetExts, 'ogg', 'm4a']));
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
